b259c6b055918f4af12a0129121098b2f99743b1
RuiHelderOliveir <rui.helderoliveira@gmail.com>
2021-05-04 17:24:59 +0100

    Implement some tests (Fragments and Navigation).

--- app/src/androidTest/java/com/example/pokeapp/fragments/LoginFragmentTest.kt
+++ app/src/androidTest/java/com/example/pokeapp/fragments/LoginFragmentTest.kt
@@ -1,6 +1,5 @@
 package com.example.pokeapp.fragments
 
-import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.Navigation
 import androidx.navigation.testing.TestNavHostController
@@ -8,17 +7,13 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.accessibility.AccessibilityChecks
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import androidx.test.platform.app.InstrumentationRegistry
 import com.example.pokeapp.R
-import com.example.pokeapp.adapters.PokemonRecyclerViewAdapter
 import com.google.common.truth.Truth.assertThat
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
-import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -27,7 +22,7 @@ import org.junit.runner.RunWith
 @HiltAndroidTest
 @RunWith(AndroidJUnit4::class)
 @LargeTest
-class PokemonListFragmentTest {
+class LoginFragmentTest {
 
     @Before
     fun setUp() {
@@ -37,53 +32,23 @@ class PokemonListFragmentTest {
     }
 
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        Assert.assertEquals("com.example.pokeapp", appContext.packageName)
-    }
-
-    @Test
-    fun getPokemons() {
-    }
-
-    @Test
-    fun setPokemons() {
-    }
-
-    @Test
-    fun getAdapter() {
-    }
-
-    @Test
-    fun setAdapter() {
-    }
-
-    @Test
     fun onCreateView() {
-    }
-
-    @Test
-    fun onClick() {
     }
 
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
 
     @Test
-    fun testNavigationToDetail() {
+    fun testNavigationToList() {
         // Create a TestNavHostController
         val navController = TestNavHostController(
-            ApplicationProvider.getApplicationContext())
-
-        val fragmentArgs = Bundle().apply {
-            putString(PokemonDetailFragment.ARG_ITEM_ID, "")
-        }
+            ApplicationProvider.getApplicationContext()
+        )
 
         // Create a graphical FragmentScenario for the TitleScreen
-        val listScenario = launchFragmentInContainer<PokemonListFragment>(fragmentArgs)
+        val loginScenario = launchFragmentInContainer<LoginFragment>()
 
-        listScenario.onFragment { fragment ->
+        loginScenario.onFragment { fragment ->
             // Set the graph on the TestNavHostController
             navController.setGraph(R.navigation.nav_graph)
 
@@ -91,7 +56,7 @@ class PokemonListFragmentTest {
             Navigation.setViewNavController(fragment.requireView(), navController)
         }
 
-        onView(withId(R.id.pokemon_list)).perform(actionOnItemAtPosition<PokemonRecyclerViewAdapter.ViewHolder>(1, click()))
+        onView(withId(R.id.login_button)).perform(click())
 
         // Confirm nav to DetailFragment and display title
         assertThat(navController.currentDestination?.id).isEqualTo(R.id.pokemon_list)
---
+++ app/src/main/java/com/example/pokeapp/activities/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.example.pokeapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
